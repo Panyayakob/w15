0f124d5d013d0cb85ab33fdd94d22bac4200dabb
Panyayakob <Turkeeyakob@gmail.com>
2024-10-18 16:11:53 +0700

    Add

---
+++ Helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace Helloworld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        c = Add(a, b);
+        Console.WriteLine($"Result of Add({a},{b} is {c})");
+    }
+    static int Add(int var1, int var2)
+    {
+        return var1 + var2;      
+    }
+}
